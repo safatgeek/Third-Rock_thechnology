4e769b0cb5c2ddc1fb24dadcd44cf926ef20a65e
safatgeek <safatd310e@gmail.com>
2023-12-23 12:52:55 +0600

    About, SectionHeading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Intro from './../components/Intro';
 import SectionDivider from '@/components/SectionDivider';
+import About from '@/components/About';
 
 export default function Home() {
   return (
     <main className='flex flex-col items-center px-4'>
       <Intro />
       <SectionDivider />
+      <About />
     </main>
   )
 }
