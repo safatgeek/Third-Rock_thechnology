22a9d04ce2a2ee45bbc0f434059ab4541ef0d71d
safatgeek <safatd310e@gmail.com>
2024-01-23 13:17:20 +0600

    Experience page using react-vertical-timeline-component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import SectionDivider from '@/components/SectionDivider';
 import About from '@/components/About';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
+import Experience from '@/components/Experience';
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <About />
       <Projects />
       <Skills />
+      <Experience />
      
     </main>
   )
